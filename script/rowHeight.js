7fdd60fec6c96a202506602438bb15733c2e5538
hungbie <a0112086@u.nus.edu>
2017-01-25 21:19:17 +0800

    Add files via upload

---
+++ script/rowHeight.js
@@ -0,0 +1,11 @@
+oldScore = $("#ranktable tbody").find("tr:first").find("td:eq(12)").text();
+oldScore = parseFloat(oldScore);
+$('#ranktable tbody tr').each(function(){
+    var currentRow = $(this).closest("tr");
+    var curScore = currentRow.find("td:eq(12)").text();
+    curScore = parseFloat(curScore);
+    var gap = oldScore - curScore;
+    oldScore = curScore;
+    var newHeight = 30 + 100 * gap;
+    $(this).closest("tr").css('height', newHeight);
+})
