dc7a77582f33d78f807e17239f51f8026e695907
hungbie <a0112086@u.nus.edu>
2017-01-29 10:43:51 +0800

    Create hightlightCell2.js

---
+++ script/hightlightCell2.js
@@ -0,0 +1,21 @@
+//find max value
+for (var i =4; i <= 12; i++) {
+	curMax = $("#ranktable tbody").find("tr:first").find('td:eq('+i+')').text();
+	curMax = parseFloat(curMax);
+
+  $('#ranktable tbody tr').each(function(){
+      var currentRow = $(this).closest("tr");
+      var curScore = currentRow.find('td:eq('+i+')').text();
+      curScore = parseFloat(curScore);
+      if (curScore > curMax) curMax = curScore;
+	alert (curMax);
+  });
+
+//Fill in max cells
+  $('#ranktable tbody tr').each(function(){
+      var currentRow = $(this).closest("tr");
+      var curScore = currentRow.find('td:eq(' + i + ')').text();
+	 curScore = parseFloat(curScore);
+      if (curScore == curMax) $(this).closest("tr").find('td:eq('+i+')').css("backgroundColor", "orange");
+  });
+}
