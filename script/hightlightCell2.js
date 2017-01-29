5959b2f64aae29c3e0999e1c5f888f6c889b3f80
hungbie <a0112086@u.nus.edu>
2017-01-29 10:44:58 +0800

    Update hightlightCell2.js

--- script/hightlightCell2.js
+++ script/hightlightCell2.js
@@ -1,8 +1,7 @@
-//find max value
 for (var i =4; i <= 12; i++) {
 	curMax = $("#ranktable tbody").find("tr:first").find('td:eq('+i+')').text();
 	curMax = parseFloat(curMax);
-
+//find max value
   $('#ranktable tbody tr').each(function(){
       var currentRow = $(this).closest("tr");
       var curScore = currentRow.find('td:eq('+i+')').text();
